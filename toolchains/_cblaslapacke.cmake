f87554c57707023e4f9fca9afe8d022d6ec399b4
Eduard Valeyev <eduard@valeyev.net>
2020-09-21 16:55:02 -0400

    generic cblas/lapacke needs to add MADNESS_LINALG_USE_LAPACKE to LAPACK_COMPILE_DEFINITIONS

--- toolchains/_cblaslapacke.cmake
+++ toolchains/_cblaslapacke.cmake
@@ -1,1 +1,2 @@
 set(LAPACK_LIBRARIES "-llapacke" "-lcblas" "-llapack" "-lblas" CACHE STRING "LAPACK libraries")
+set(LAPACK_COMPILE_DEFINITIONS "MADNESS_LINALG_USE_LAPACKE" CACHE STRING "LAPACK preprocessor definitions")
