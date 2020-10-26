a5d8525eb4dca6bcd1a19bd0bf37a9d1de48075c
Eduard Valeyev <eduard@valeyev.net>
2020-10-26 13:43:24 -0400

    add -L/usr/lib/x86_64-linux-gnu to blacs_LIBRARIES in travis toolchain

--- toolchains/travis.cmake
+++ toolchains/travis.cmake
@@ -23,6 +23,6 @@ set(BLA_STATIC OFF CACHE BOOL "Whether to use static linkage for BLAS, LAPACK, a
 # for wavefunction91's FindLAPACK
 set( lapack_LIBRARIES ${BLAS_LINKER_FLAGS} )
 # BLACS
-set( blacs_LIBRARIES      "-L$ENV{INSTALL_PREFIX}/scalapack/lib;-lscalapack;${lapack_LIBRARIES};-lgfortran" )
+set( blacs_LIBRARIES      "-L$ENV{INSTALL_PREFIX}/scalapack/lib;-lscalapack;${lapack_LIBRARIES};-L/usr/lib/x86_64-linux-gnu;-lgfortran" )
 # ScaLAPACK
 set( scalapack_LIBRARIES  "${blacs_LIBRARIES}" )
