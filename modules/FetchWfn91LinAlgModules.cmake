a83e5899af658b7b5158720fff7d83f6fb5fc534
Eduard Valeyev <eduard@valeyev.net>
2021-02-11 09:59:17 -0500

    enable C language, if not enabled + do not fetch inside possibly-fetchcontented source

--- modules/FetchWfn91LinAlgModules.cmake
+++ modules/FetchWfn91LinAlgModules.cmake
@@ -1,13 +1,21 @@
 # - fetches github.com/wavefunction91/linalg-cmake-modules
 
-cmake_minimum_required (VERSION 3.11.0)  # FetchContent
+# prerequisites:
+# 1. cmake with FetchContent
+cmake_minimum_required (VERSION 3.11.0)
+# 2. wavefunction91_linalg_kit try_compile's C code
+get_property(_enabled_languages GLOBAL PROPERTY ENABLED_LANGUAGES)
+if (NOT C IN_LIST _enabled_languages)
+    enable_language(C)
+endif(NOT C IN_LIST _enabled_languages)
+
+# download wavefunction91_linalg_kit, if needed
 include(FetchContent)
 FetchContent_Declare(
         wavefunction91_linalg_kit
         QUIET
         GIT_REPOSITORY      https://github.com/wavefunction91/linalg-cmake-modules.git
         GIT_TAG    81fa1754dc592a0b6bde98abd650a8270d74fa1b
-        SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/wfn91-linalg
 )
 FetchContent_GetProperties(wavefunction91_linalg_kit)
 if(NOT wavefunction91_linalg_kit_POPULATED)
