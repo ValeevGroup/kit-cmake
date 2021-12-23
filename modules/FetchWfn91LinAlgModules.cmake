6586c1d18027f97e034d5d4ebbf4164e973f32ed
Eduard Valeyev <eduard@valeyev.net>
2021-12-23 13:58:28 -0500

    linalg-cmake-modules: change FC project name from wavefunction91_linalg_kit to linalg-cmake-modules

--- modules/FetchWfn91LinAlgModules.cmake
+++ modules/FetchWfn91LinAlgModules.cmake
@@ -1,24 +1,24 @@
-# - fetches github.com/wavefunction91/linalg-cmake-modules
+# - fetches github.com/ajaypanyala/linalg-cmake-modules
 
 # prerequisites:
 # 1. cmake with FetchContent
 cmake_minimum_required (VERSION 3.11.0)
-# 2. wavefunction91_linalg_kit try_compile's C code
+# 2. linalg-cmake-modules try_compile's C code
 get_property(_enabled_languages GLOBAL PROPERTY ENABLED_LANGUAGES)
 if (NOT C IN_LIST _enabled_languages)
     enable_language(C)
 endif(NOT C IN_LIST _enabled_languages)
 
-# download wavefunction91_linalg_kit, if needed
+# download linalg-cmake-modules, if needed
 include(FetchContent)
 FetchContent_Declare(
-        wavefunction91_linalg_kit
+        linalg-cmake-modules
         QUIET
         GIT_REPOSITORY  https://github.com/ajaypanyala/linalg-cmake-modules.git 
         GIT_TAG         f6629057033a9dd31416b259f83233340106fa78 
 )
-FetchContent_GetProperties(wavefunction91_linalg_kit)
-if(NOT wavefunction91_linalg_kit_POPULATED)
-    FetchContent_Populate(wavefunction91_linalg_kit)
-    list(APPEND CMAKE_MODULE_PATH "${wavefunction91_linalg_kit_SOURCE_DIR}")
-endif(NOT wavefunction91_linalg_kit_POPULATED)
+FetchContent_GetProperties(linalg-cmake-modules)
+if(NOT linalg-cmake-modules_POPULATED)
+    FetchContent_Populate(linalg-cmake-modules)
+    list(APPEND CMAKE_MODULE_PATH "${linalg-cmake-modules_SOURCE_DIR}")
+endif(NOT linalg-cmake-modules_POPULATED)
